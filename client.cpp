51e86b776c1640c427531e0577f2133debd11763
joelsare <60672302+joelsare@users.noreply.github.com>
2020-03-06 18:19:40 -0600

    Bare Bones UDP Server

--- client.cpp
+++ client.cpp
@@ -1,8 +1,96 @@
-#include <string>
-#include <thread>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
 #include <iostream>
+#include <sstream>
+#include <netdb.h>
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
 
-int main()
+using namespace std; 
+
+
+int main(int argc, char *args[])
 {
-  std::cerr << "client is not implemented yet" << std::endl;
+  struct timeval timeout;
+  timeout.tv_sec = 10;
+  timeout.tv_usec = 0;
+
+  char * hostName = args[1];
+
+  stringstream p(args[2]);
+  int port;
+  p >> port;
+
+  if ((port <= 1023))
+  {
+    std::cerr << "ERROR: invalid arguments" << std::endl;
+    return 1;
+  }
+
+  if (gethostbyname(hostName) == NULL)
+  {
+    std::cerr << "ERROR: invalid arguments" << std::endl;
+    return 1;
+  }
+
+  char * fileName = args[3];
+
+  // create a socket using UDP IP
+  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+  // struct sockaddr_in addr;
+  // addr.sin_family = AF_INET;
+  // addr.sin_port = htons(40001);     // short, network byte order
+  // addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  // memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
+  // if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+  //   perror("bind");
+  //   return 1;
+  // }
+
+  struct sockaddr_in serverAddr;
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(port);     // short, network byte order
+  serverAddr.sin_addr.s_addr = inet_addr(hostName);
+  memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
+
+  struct sockaddr_in clientAddr;
+  socklen_t clientAddrLen = sizeof(clientAddr);
+  if (getsockname(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLen) == -1)
+  {
+    perror("getsockname");
+    return 3;
+  }
+
+  if (setsockopt (sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0)
+  {
+    cerr << "Error";
+    return 1;
+  }
+
+  char ipstr[INET_ADDRSTRLEN] = {'\0'};
+  inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
+
+
+  // send/receive data to/from connection
+  char buf[20] = {0};
+  std::stringstream ss;
+  unsigned int n, len;
+
+  memset(buf, '\0', sizeof(buf));
+
+  sendto(sockfd, fileName, sizeof(fileName), MSG_SEND, (const struct sockaddr *) &serverAddr, sizeof(serverAddr)); 
+        
+  n = recvfrom(sockfd, buf, sizeof(buf), MSG_DONTWAIT, (struct sockaddr *) &serverAddr, &len); 
+  //printf("Server : %s\n", buf);
+  close(sockfd);
+  return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,8 +1,90 @@
-#include <string>
-#include <thread>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sstream> 
 #include <iostream>
 
-int main()
+int main(int argc, char *args[])
 {
-  std::cerr << "server is not implemented yet" << std::endl;
+  int counter = 0;
+  int port;
+  int new1;
+  int val;
+  int bytes_read;
+  char buf[20] = {0};
+  struct sockaddr_in addr;
+
+  std::stringstream p(args[1]);
+  p >> port;
+  if ((port <= 1023))
+  {
+    std::cerr << "ERROR: port must be above 1023" << std::endl;
+    return 1;
+  }
+
+  // create a socket using UDP IP
+  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+  // allow others to reuse the address
+  int yes = 1;
+  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+  {
+    perror("setsockopt");
+    return 1;
+  }
+
+  // bind address to socket
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(port);     // short, network byte order
+  addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
+
+  if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1)
+  {
+    perror("bind");
+    return 2;
+  }
+
+  while(1)
+  {
+    memset(buf, '\0', sizeof(buf));      
+    struct sockaddr_in clientAddr;
+    socklen_t clientAddrSize = sizeof(clientAddr);
+
+    //new1 = accept(sockfd, (struct sockaddr*)&clientAddr, &clientAddrSize);
+    //char ipstr[INET_ADDRSTRLEN] = {'\0'};
+    //inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
+    bytes_read = recvfrom(sockfd, buf, sizeof(buf), MSG_DONTWAIT, ( struct sockaddr *) &clientAddr, &clientAddrSize); 
+    if (bytes_read > 0)
+    {
+      val = fork();
+      if (val == -1)
+      {
+        close(new1);
+        continue;
+      }
+      else if (val > 0)
+      {
+        close(new1);
+        counter++;
+        continue;
+      }
+      else if (val == 0)
+      {
+        printf("Client #%d : %s\n", counter, buf); 
+        sendto(sockfd, buf, sizeof(buf), MSG_SEND, (const struct sockaddr *) &clientAddr, clientAddrSize); 
+
+        counter++;
+        close(new1);
+        break;
+      }
+    }
+  }
+  close(new1);
+  return 0;
 }
