c58cdc812592f2acb7687ed55a7f8602a4bb1c38
joelsare <60672302+joelsare@users.noreply.github.com>
2020-03-18 13:36:41 -0500

    Saves files up to 512bytes W Multi-Threading

--- server.cpp
+++ server.cpp
@@ -11,19 +11,63 @@
 #include <fstream>
 #include "header.h"
 
+char h[12];
+char file[512];
+char buf[524] = {0};
+struct header a;
+int CWND = 512;
+int SS_THRESH = 10000;
+std::string dirName;
+std::string destination;
+int sockfd;
+struct sockaddr_in clientAddr;
+
+void *connection_handler(void *socket_desc)
+{
+  int sock = *(int*)socket_desc;
+  std::ofstream fp;
+  static int count = 0;
+  memcpy(&a, buf, sizeof(a));
+  memcpy(&file, buf + sizeof(a), sizeof(file));
+
+  if (a.S == 1 && a.F == 0 && a.A == 0)
+  {
+    a.awknum = a.sequencenum + 1;
+    a.A = 1;
+    a.connid = count++;
+    a.sequencenum = 4321;
+  }
+  else if (a.S == 0 && a.F == 0 && a.A == 0)
+  {
+    destination = dirName + std::to_string(count) + ".file";
+    printf("File: %s\n",file);
+    printf("File length: %lu\n", strlen(file));
+    int len = strlen(file);
+    if(len > 512)
+      len = 512;
+    fp.open(destination,std::ofstream::app);
+    for (int i = 0; i < len; i++)
+    {
+      fp << file[i];
+    }
+    fp.close();
+  }
+  memcpy(buf, &a, sizeof(a));
+  memcpy(buf + sizeof(a), file, sizeof(file));
+  sendto(sockfd, buf, sizeof(buf), MSG_SEND, (const struct sockaddr *) &clientAddr, sizeof(clientAddr)); 
+
+  close(sock);
+  return 0;
+}
+
 int main(int argc, char *args[])
 {
   const int MAX = 102400;
-  int CWND = 512;
-  int SS_THRESH = 10000;
   int counter = 0;
   int port;
   int new1;
   int val;
   int bytes_read;
-  char buf[524] = {0};
-  char h[12];
-  char file[512];
   struct sockaddr_in addr;
 
   std::stringstream p(args[1]);
@@ -34,11 +78,11 @@ int main(int argc, char *args[])
     return 1;
   }
 
-  std::string dirName = args[2];
+  dirName = args[2];
   dirName = dirName.substr(1) + "/";
 
   // create a socket using UDP IP
-  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
   // allow others to reuse the address
   int yes = 1;
@@ -60,25 +104,31 @@ int main(int argc, char *args[])
     return 2;
   }
   
-  std::string destination;
-  std::ofstream fp;
-
-  while(1)
+  pthread_t thread_id;
+  socklen_t clientAddrSize = sizeof(clientAddr);
+  memset(buf, '\0', sizeof(buf));      
+  while((bytes_read = recvfrom(sockfd, buf, sizeof(buf), MSG_DONTWAIT, ( struct sockaddr *) &clientAddr, &clientAddrSize)))
   {
-    struct header a;
-
-    memset(buf, '\0', sizeof(buf));      
-    struct sockaddr_in clientAddr;
-    socklen_t clientAddrSize = sizeof(clientAddr);
-
+    if (bytes_read > 0)
+    {
+      bytes_read = 0;
+      if( pthread_create( &thread_id , NULL ,  connection_handler , (void*) &new1) < 0)
+      {
+        puts("Could not create thread");
+        pthread_join( thread_id , NULL);
+      }
+    }
+  }
+  close(new1);
+  return 0;
+}
+/*
     //new1 = accept(sockfd, (struct sockaddr*)&clientAddr, &clientAddrSize);
     //char ipstr[INET_ADDRSTRLEN] = {'\0'};
     //inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
     bytes_read = recvfrom(sockfd, buf, sizeof(buf), MSG_DONTWAIT, ( struct sockaddr *) &clientAddr, &clientAddrSize); 
     if (bytes_read > 0)
     {
-      memcpy(&a, buf, sizeof(a));
-      memcpy(file, buf + sizeof(h), sizeof(file));
       val = fork();
       if (val == -1)
       {
@@ -93,37 +143,8 @@ int main(int argc, char *args[])
       }
       else if (val == 0)
       {
-        if (a.S == 1 && a.F == 0 && a.A == 0)
-        {
-          printf("This should only appear once\n");
-          printf("%d %d %d %d %d\n", a.sequencenum, a.awknum, a.connid, CWND, SS_THRESH);
-          printf("Counter: %d\n", counter);
-          a.awknum = a.sequencenum + 1;
-          a.A = 1;
-          a.connid = counter;
-          a.sequencenum = 4321;
-        }
-        else if (a.S == 0 && a.F == 0 && a.A == 0)
-        {
-          destination = dirName + std::to_string(counter) + ".file";
-          printf("File: %s\n",file);
-          fp.open(destination,std::ofstream::app);
-          for (int i; i < strlen(file); i++)
-          {
-            fp << file[i];
-          }
-          fp.close();
-        }
-        counter++;
-        memcpy(buf, &a, sizeof(a));
-        memcpy(buf + sizeof(a), file, sizeof(file));
-        sendto(sockfd, buf, sizeof(buf), MSG_SEND, (const struct sockaddr *) &clientAddr, clientAddrSize); 
-
-        close(new1);
+      
         break;
       }
     }
-  }
-  close(new1);
-  return 0;
-}
+    */
